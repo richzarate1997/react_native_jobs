d9287bb2410db9335f9cf12e7e47c7edc0c3abbe
richzarate1997 <richzarate97@gmail.com>
2023-07-24 14:06:24 -0700

    start job-details

--- job-search/hook/useFetch.js
+++ job-search/hook/useFetch.js
@@ -1,9 +1,6 @@
 
 import { useState, useEffect } from "react";
 import axios from "axios";
-// import {RAPID_API_KEY} from '@env';
-
-// const rapidApiKey = RAPID_API_KEY;
 
 const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
@@ -14,7 +11,7 @@ const useFetch = (endpoint, query) => {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-            'X-RapidAPI-Key': '',
+            'X-RapidAPI-Key': '',//process.env.RAPID_API_KEY,
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         },
         params: {...query},
